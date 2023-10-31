280050697b6796154e5e4a1dbc798cda8b22c513
Francsy <francsyludens@gmail.com>
2023-10-31 19:48:34 +0100

    improve/solution

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const cargador = ["pium!", "pium!", "pium!", "pium!", "pium!", "pium!", "pium!"]
 
 
 function chauchat(carga) {
-    for (i = 0; i < carga.length; i++) {
-        if (Math.random() * 100 > 80) {
-            console.log("Illo, me he quedao pillá!");
-        } else {
-            for (i = 0; i < carga.length; i++) {
-                if (i % 3 == 0) {
-                    console.log("\n")
-                    console.log(carga[i])
-                } else {
-                    console.log(carga[i]);
-                }
+    if (Math.random() * 100 > 80) {
+        console.log("Illo, me he quedao pillá!");
+    } else {
+        for (i = 0; i < carga.length; i++) {
+            if (i % 3 == 0 && i !==0) {
+                console.log("\n")
+                console.log(carga[i])
+            } else {
+                console.log(carga[i]);
             }
         }
     }
 }
+
+// One line solution:
+
+const chauchatV2 = (carga) => Math.random() * 100 > 80 ? console.log("Illo, me he quedao pillá!") : carga.forEach((bullet, i) => i % 3 == 0 && i !== 0 ? console.log("\n" + bullet) : console.log(bullet))
 
